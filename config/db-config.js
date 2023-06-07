e5c53d2f8f79d189d40e257ebd6e64e07526d440
Hassan <hassan.sweden.dev@gmail.com>
2023-06-07 10:13:01 +0200

    Update db-config.js

--- config/db-config.js
+++ config/db-config.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 async function connectToDB() {
   try {
     const { connection } = await mongoose.connect(process.env.CONNECTION);
-    console.info("[db]: Connected:", connection.host, connection.name);
+    console.info(`[db]: Connected to ${connection.host}/${connection.name}`);
   } catch (error) {
     console.error("Could not connect to DB");
     process.exitCode = 1;
