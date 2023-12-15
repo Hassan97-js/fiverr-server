785a556370409d4fca387ae1a03350185f1cd77a
Hassan97-js <hassan.sweden.dev@gmail.com>
2023-12-15 17:28:06 +0100

    npm update

--- utils/db.utils.js
+++ utils/db.utils.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 export const connectToDB = async () => {
   try {
-    const { connection } = await mongoose.connect(process.env.CONNECTION);
+    const { connection } = await mongoose.connect(process.env.DB_URI);
     console.info(`[db]: Connected to ${connection.host}/${connection.name}`);
   } catch (error) {
     console.error("Could not connect to DB");
