e3948069d90690a686c1581d629fb60075340810
Hassan97-js <hassan.sweden.dev@gmail.com>
2024-01-21 09:35:07 +0100

    update logger.ts

--- src/constants/logger.ts
+++ src/constants/logger.ts
@@ -1,14 +1,19 @@
 import { createLogger, format, transports } from "winston";
+import winston from "winston/lib/winston/transports";
 
 export const logger = createLogger({
   level: "info",
-  format: format.combine(
-    format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss",
-    }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json()
-  ),
-  defaultMeta: { service: "your-service-name" },
+  defaultMeta: { service: "" },
+  transports: [
+    new winston.Console({
+      format: format.combine(
+        format.timestamp({
+          format: "YYYY-MM-DD HH:mm:ss"
+        }),
+        format.errors({ stack: true }),
+        format.splat(),
+        format.json()
+      )
+    })
+  ]
 });
